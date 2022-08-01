1b7240f69e85df0abdf961329b89540f8a7fb6c3
skyv26 <dev.aakashv@gmail.com>
2022-08-01 19:56:38 +0530

    Add the `files`

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const bookObject = {};
+const preserveDataList = [];
+
+const localStorageHandler = () => {
+
+};
+
+const objIteratorHandler = () => {
+
+};
+
+const addBook = () => {
+
+};
+
+const removeBook = () => {
+
+};
