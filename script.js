fb38971edcc18c3efa3797c5f9bda1f426768032
shella12 <ayeshaarshad4567@gmail.com>
2022-08-01 19:47:36 +0500

    Add Jscript

--- script.js
+++ script.js
@@ -2,6 +2,28 @@
 const bookObject = {};
 const preserveDataList = [];
 
+const bookStoreSection=document.querySelector('.book-store');
+
+const uList=document.createElement('ul');
+uList.className="uList";
+bookStoreSection.appendChild(uList);
+
+const list=document.createElement('li');
+list.className="list";
+uList.appendChild(list);
+
+const bookTitle=document.createElement('h2');
+bookTitle.className="bookTitle";
+list.appendChild(bookTitle);
+
+const bookAuthor=document.createElement('h3');
+bookAuthor.className="bookAuthor";
+list.appendChild(bookAuthor);
+
+const removeButton=document.createElement('button');
+removeButton.className="remove-btn";
+list.appendChild(removeButton);
+
 const localStorageHandler = () => {
 
 };
